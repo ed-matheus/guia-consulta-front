61146753f1d751bb3bb28c595e5769fc5a9ac7e6
ed-matheus <edsonmatheus02@hotmail.com>
2024-07-02 14:34:33 -0300

    progresso na construção da navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 
 // Componentes
 import Sidebar from './components/sidebar/Sidebar'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-    <div className='App'>
-      <Sidebar/>
+    <div className='App d-flex'>
+      <Sidebar />
+      <Navbar />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import './Navbar.css'
+import userAvatar from '/avatar.png'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='navbar bg-body-tertiary w-100 px-3'>
+            <div className='container-fluid'>
+                <span className='display-6'>Página inicial</span>
+
+                <div className='user_info d-flex align-items-center'>
+                    <span className='fw-bold fs-5'>Username</span>
+                    <div className='avatar ms-3'>
+                        <img src={userAvatar} alt="" width={50} />
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
